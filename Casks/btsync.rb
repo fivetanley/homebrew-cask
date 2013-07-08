b8dce95854f9d1b056fab71dea5c68781e6fbf28
Stanley Stuart <dstanley.stuart@gmail.com>
2013-07-08 16:47:53 -0500

    add btsync to Casks

---
+++ Casks/btsync.rb
@@ -0,0 +1,7 @@
+class BTSync < Cask
+  url 'http://btsync.s3-website-us-east-1.amazonaws.com/BTSync.dmg'
+  homepage 'http://labs.bittorrent.com/experiments/sync.html'
+  version '1.1.27'
+  no_checksum
+  link 'BitTorrent Sync.app'
+end
